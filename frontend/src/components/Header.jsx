3b746a0e78a5e51bffad64de2e348003ad1aa6c9
Chayanka <chayanka2318@gmail.com>
2025-01-17 16:02:02 +0530

    -

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,32 +1,36 @@
-import { Link } from 'react-router-dom'
-import { Facebook, Twitter, Linkedin } from 'lucide-react'
+import { Link } from "react-router-dom";
+import { Facebook, Twitter, Linkedin } from "lucide-react";
 
 function Header() {
   return (
     <header className="bg-white shadow-sm">
       <div className="container flex items-center justify-between px-4 py-4 mx-auto">
         <div className="flex items-center space-x-6">
-          <Link to="/" className="text-xl font-medium text-orange-500">Home</Link>
-          <Link to="/jobs" className="text-gray-600 hover:text-orange-500">Job List</Link>
+          <Link to="/" className="text-xl font-medium text-orange-500">
+            Home
+          </Link>
+          <Link to="/jobs" className="text-gray-600 hover:text-orange-500">
+            Job List
+          </Link>
         </div>
-        
+
         <div className="flex items-center space-x-6">
           <div className="flex items-center">
-            <Link 
-              to="/signup" 
+            <Link
+              to="/signup"
               className="px-4 py-2 text-white transition-colors bg-orange-500 rounded-md hover:bg-orange-600"
             >
               Sign Up
             </Link>
             <span className="mx-2 text-gray-400">/</span>
-            <Link 
-              to="/login" 
+            <Link
+              to="/login"
               className="px-4 py-2 text-white transition-colors bg-orange-500 rounded-md hover:bg-orange-600"
             >
               Login
             </Link>
           </div>
-          
+
           <div className="flex items-center space-x-4">
             <a href="#" className="text-gray-600 hover:text-orange-500">
               <Facebook className="w-5 h-5" />
@@ -41,8 +45,7 @@ function Header() {
         </div>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
-
+export default Header;
